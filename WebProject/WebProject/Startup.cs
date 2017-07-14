ac94cc7dd5f1303d1cc8a7a2c539486573ee0784
rsysgithub <arvind.maurya@rsystems.com>
2017-07-14 06:41:38 -0700

    Commit All Solution in One Go

---
+++ WebProject/WebProject/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebProject.Startup))]
+namespace WebProject
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
